39263a2515b52ede7e5a485a0a47893878510777
wkdeo12 <wkdeo12>
2018-11-27 22:16:24 +0900

    숙제

---
+++ Assets/AssginManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class AssginManager : MonoBehaviour {
+
+    
+    public Button Startbutton; 
+    
+    public GameObject e;
+
+	void Start () {
+	}
+
+    void Update()
+    {
+	}
+
+    public void startbutton()
+    {
+        Startbutton.gameObject.SetActive(false);
+        e.gameObject.SetActive(true);
+    }
+    
+
+
+
+}
